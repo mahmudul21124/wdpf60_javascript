3050a97c3d50fc4ae2430e241ae1e9a72f86b761
mh.ju1803@gmail.com <mh.ju1803@gmai.com>
2024-06-05 13:06:06 +0600

    05 June Classwork

---
+++ student.js
@@ -0,0 +1,11 @@
+const studentInfo = () => {
+    let name = "Rahim";
+    let address = "Mirpur";
+    return `Name: ${name} <br> Address: ${address}`;
+}
+
+let country = "Bangladesh";
+let district = "Dhaka"
+
+export default studentInfo;
+export {country, district};
